e65221b33543c4c74cc175d675cefa918ee11cdd
marcello lupo <158152797+puppydog4@users.noreply.github.com.>
2025-02-27 14:34:11 +0000

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  module.exports = {
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-  }
 };
 
 export default nextConfig;
